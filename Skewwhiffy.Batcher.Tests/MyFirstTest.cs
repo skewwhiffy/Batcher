a460c92768d9e9265c8d891443f77c578c15badd
Kenny Hung <kennysubs+github@outlook.com>
2016-11-30 11:21:46 +0000

    First test

---
+++ Skewwhiffy.Batcher.Tests/MyFirstTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skewwhiffy.Batcher.Tests
+{
+    public class MyFirstTest
+    {
+        [Test]
+        public void TestMyFirst()
+        {
+            Assert.Throws<NotImplementedException>(() => new MyFirst());
+        }
+    }
+}
