9dcc72d86e06b1f8127bc227b3d2dda6722c6cda
Kenny Hung <kennyhung@live.co.uk>
2016-11-30 11:21:46 +0000

    First test

---
+++ Skewwhiffy.Batcher/MyFirst.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Skewwhiffy.Batcher
+{
+    public class MyFirst
+    {
+        public MyFirst()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
